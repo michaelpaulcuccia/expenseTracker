5b997099bdb28edc2e56384289dfd6fdc79e7543
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2020-03-08 12:45:54 -0400

    completed

--- script.js
+++ script.js
@@ -8,14 +8,58 @@ const text = document.getElementById('text');
 const amount = document.getElementById('amount');
 
 //test data
-const dummyTransactions = [
-    { id: 1, text: 'Flowers', amount: -20 },
-    { id: 2, text: 'Salary', amount: 300 },
-    { id: 3, text: 'Book', amount: -10 },
-    { id: 4, text: 'Camera', amount: 150 }
-];
+// const dummyTransactions = [
+//     { id: 1, text: 'Flowers', amount: -20 },
+//     { id: 2, text: 'Salary', amount: 300 },
+//     { id: 3, text: 'Book', amount: -10 },
+//     { id: 4, text: 'Camera', amount: 150 }
+// ];
 
-let transactions = dummyTransactions;
+const localStorageTransactions = JSON.parse(localStorage.getItem('transactions'));
+
+//let transactions = dummyTransactions;
+//not equal to null (something in there) use, else set to an empty array
+//in Dev Tools, Application, Local Storage 
+let transactions = localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
+
+//add transaction
+function addTransaction(e){
+    e.preventDefault();
+
+    if(text.value.trim() === '' || amount.value.trim() === ''){
+        alert('please add a text and amount');
+    } else {
+        const transaction = {
+            id: generateID(),
+            text: text.value,
+            amount: +amount.value //+ symbol turns string into number
+        }
+        //console.log(transaction);
+
+         //push to transaction array
+         transactions.push(transaction)
+
+        //add to DOM
+        addTransactionDOM(transaction);
+
+        //update values
+        updateValues();
+
+        //update local storage
+        updateLocalStorage();
+
+        //clear input
+        text.value = '';
+        amount.value = '';
+         
+    }
+}
+
+//generate a random id
+function generateID() {
+    return Math.floor(Math.random() * 100000000)
+}
+
 
 //display transactions in DOM, 'HISTORY'
 function addTransactionDOM(transaction) {
@@ -30,7 +74,7 @@ function addTransactionDOM(transaction) {
     item.classList.add(transaction.amount < 0 ? 'minus' : 'plus')
 
     //Math.abs - 'In Math, ABS stands for Absolute Value. An Absolute Value basically just makes sure a number is positive.'
-    item.innerHTML = `${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span> <button class='delete-btn'>x</button>`;
+    item.innerHTML = `${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span> <button class='delete-btn' onClick='removeTransaction(${transaction.id})'>x</button>`;
 
     //add to DOM - list - UL in History, gets appended with item
     list.appendChild(item);
@@ -55,11 +99,28 @@ function updateValues() {
     //get expense 
     //set to amounts, filter out items LESS THAN zero
     const expense = (amounts.filter(item => item < 0).reduce((acc, item) => (acc += item),0) * -1).toFixed(2);
-    console.log(expense);
+    //console.log(expense);
 
     balance.innerText = `$${total}`;
     money_plus.innerText = `$${income}`;
     money_minus.innerText = `$${expense}`;
+}
+
+//remove transaction by ID
+function removeTransaction(id) {
+    //transaction ID not equal to ID passed in
+    transactions = transactions.filter(transaction => transaction.id !== id);
+
+    //update local storage
+    updateLocalStorage();
+
+    //re-initialize 
+    init();
+}
+
+//update local storage transactions
+function updateLocalStorage() {
+    localStorage.setItem('transactions', JSON.stringify(transactions))
 }
 
 //init app
@@ -74,6 +135,8 @@ function init() {
 }
 
 init();
+
+form.addEventListener('submit', addTransaction);
 
 
 
